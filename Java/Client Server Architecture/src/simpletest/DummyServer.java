defde0b089503245d69799ac60e382e90f3a7a89
David Ilunga <84725985+davidilunga@users.noreply.github.com>
2022-05-27 13:04:30 +0100

    Add files via upload

---
+++ Java/Client Server Architecture/src/simpletest/DummyServer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpletest;
+
+/**
+ *
+ * @author david
+ */
+public class DummyServer {
+    
+    String testConnection(){
+        System.out.println("DummyServer. Connection being tested ... ");
+        return "Conection Succeeded";
+    }
+}
